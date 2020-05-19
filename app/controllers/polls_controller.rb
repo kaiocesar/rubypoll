77f687c2449884a3eceb5e0c5060b821734fca78
Kaio Cesar <tecnico.kaio@gmail.com>
2020-05-19 00:13:18 -0300

    fix routes

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -6,6 +6,15 @@ class PollsController < ApplicationController
     end
 
     def show
+        @poll = {
+            "poll_id": 1,
+            "poll_description": "This is the description",
+            "options": [
+                {"option_id": 1, "option_description": "First option"},
+                {"option_id": 2, "option_description": "Second option"},
+                {"option_id": 3, "option_description": "Third option"},
+            ]
+        }
     end
 
     def stats
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # defaults format: :json do
-  resources :polls, path: :poll, only: %i[show create]
-  get '/poll/:id/stats', to: 'polls#stats'
-  post '/poll/:id/vote', to: 'polls#vote'
-  # end
+  defaults format: :json do
+    resources :polls, path: :poll, only: %i[show create]
+    get '/poll/:id/stats', to: 'polls#stats'
+    post '/poll/:id/vote', to: 'polls#vote'
+  end
 end
